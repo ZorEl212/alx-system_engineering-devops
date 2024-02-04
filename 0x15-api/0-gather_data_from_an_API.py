0052efd1daca879b482b5778021429056401f4d7
ZorEl212 <yabsirad212@proton.me>
2024-02-04 15:10:25 +0300

    Fix pycodestyle
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,30 +6,29 @@ from sys import argv
 
 def emp_status(emp_id):
     res = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-    emp = requests.get(f"https://jsonplaceholder.typicode.com/users/{emp_id}").json()
+    emp = requests.get(f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+                       ).json()
 
     emp_tasks = ()
     tasks = []
     emp_name = emp['name']
     for item in res:
-            if item['userId'] == int(emp_id):
-                tasks.append(item)
+        if item['userId'] == int(emp_id):
+            tasks.append(item)
 
     for task in tasks:
-        if task['completed'] == True:
+        if task['completed'] is True:
             emp_tasks += (task['title'],)
 
     print(f"Employee {emp_name} is done with tasks"
-        f"({len(emp_tasks)}/{len(tasks)}):")
-
+          f"({len(emp_tasks)}/{len(tasks)}):")
 
     for t in emp_tasks:
         print(f"\t {t}")
+
 
 if __name__ == '__main__':
     if len(argv) < 2:
         print("Please input employee id")
         exit()
     emp_status(argv[1])
-    
-    
